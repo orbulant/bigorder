bb8c26a36392ebbe90d66d2120021d5dae827f19
Tan Ka-Shing <shing@shing-macbook-pro16.local>
2022-07-09 03:53:25 +0800

    Fixed Public Menu Backend bug

--- backend/controllers/publicMenuController.js
+++ backend/controllers/publicMenuController.js
@@ -9,7 +9,13 @@ const getPublicMenu = asyncHandler(async (req, res) => {
     let menu = await Menu.findOne(
         { _id: req.params.id },
         "-user -createdAt -updatedAt -__v"
-    ).sort({menuItems: {name: 1}});
+    );
+
+    menu.menuItems.sort((a, b) => {
+        const alc = a.name.toLowerCase(),
+            blc = b.name.toLowerCase();
+        return alc > blc ? 1 : alc < blc ? -1 : 0;
+    });
 
     res.status(200).json(menu);
 });
